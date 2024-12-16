077f6e4a0e2b8e06a761f4ca40e78fbcf01523a3
jubil reji <rejijubil@gmail.com>
2024-12-16 23:55:40 +0530

    ADD

---
+++ index.js
@@ -0,0 +1,81 @@
+/* 
+const leng = 8
+const bridth = 9
+
+const area = leng * bridth;
+console.log(area)
+alert(area)
+
+//for loop
+
+for(let n = 1; n<5;  n++){
+    console.log(n)
+} */
+
+/* for( let i = 1; i<=3 ; i++){
+  for(let j = 1 ; j <=i ; j++){
+   
+  }
+} */
+
+//for
+//while
+//do while
+//foreach
+
+
+/* const studetInClass = [
+  { 
+    name:'jubil',
+    age:18
+  },
+  {
+    name:'jaya',
+    age:19
+  },
+  {
+    name:'jisa',
+    age:20
+  }
+]
+
+studetInClass.forEach(
+  (items)=>{
+  }
+) */
+/* let weight = 10;
+let height = 10;
+let totalHeight = 0
+let totalWeight  = 0
+
+function calcuLate( heights , weights ){
+ totalHeight =  weight*weights;
+ totalWeight= height*heights;
+ 
+ 
+  return calcuLate ;
+}
+
+calcuLate(7,5)
+console .log(totalHeight)
+console.log(totalWeight); */
+ 
+
+
+
+//18 plus only apply for licence
+
+/*  let age = 9 ;
+ let uREligble = [];
+ 
+ if( age <= 18   || age > 18  ){
+    console.log('u are eligible for apply licence'|| 'ur n e')
+ }
+
+
+ for ( let i = 10 ; i<20; i++){
+     if( i / 2){
+       i++
+       }
+       console.log (i)
+ } */
